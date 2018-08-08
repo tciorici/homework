c2ec08f892e30b0dbfb69fc54d66b0690a132b43
tciorici <teodor.ciorici@yahoo.com>
2018-08-08 19:51:18 +0300

    Added exercise 2_18, Cub, Oval etc

---
+++ ExerciseCub/ExerciseCub.java
@@ -0,0 +1,15 @@
+public class ExerciseCub{
+	public static void main(String args[] ){
+	    System.out.println("Urmeaza magia");
+	    System.out.println("");
+		System.out.println("*********" + "   " + "   ***   " + " " + "    *    " + " " + "    *    ");
+		System.out.println("*       *" + "   " + " *     * " + " " + "   ***   " + " " + "   * *   ");
+		System.out.println("*       *" + "   " + "*       *" + " " + "  *****  " + " " + "  *   *  ");
+		System.out.println("*       *" + "   " + "*       *" + " " + "    *    " + " " + " *     * ");
+		System.out.println("*       *" + "   " + "*       *" + " " + "    *    " + " " + "*       *");
+		System.out.println("*       *" + "   " + "*       *" + " " + "    *    " + " " + " *     * ");
+		System.out.println("*       *" + "   " + "*       *" + " " + "    *    " + " " + "  *   *  ");
+		System.out.println("*       *" + "   " + " *     * " + " " + "    *    " + " " + "   * *   ");
+		System.out.println("*********" + "   " + "   ***   " + " " + "    *    " + " " + "    *    ");
+	}	
+}
