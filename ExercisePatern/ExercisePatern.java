18137b4b6f056f2e8836e32d748705d3c9f2ec5a
tciorici <teodor.ciorici@yahoo.com>
2018-08-08 20:02:53 +0300

    Added exercise2_27 Patern

---
+++ ExercisePatern/ExercisePatern.java
@@ -0,0 +1,14 @@
+public class ExercisePatern{
+	public static void main(String args[] ){
+	    System.out.println("Urmeaza magia");
+	    System.out.println("");
+		System.out.println("* * * * * * * *");
+		System.out.println(" * * * * * * * *");
+		System.out.println("* * * * * * * *");
+		System.out.println(" * * * * * * * *");
+		System.out.println("* * * * * * * *");
+		System.out.println(" * * * * * * * *");
+		System.out.println("* * * * * * * *");
+		System.out.println(" * * * * * * * *");
+	}	
+}
